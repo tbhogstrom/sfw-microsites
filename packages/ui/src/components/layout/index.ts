7cc967395a049d0ac5f9cc34d2bf97fab9d71c88
Tyler Falcon <tfalcon@sfwconstruction.com>
2026-02-23 08:20:15 -0800

    Add CCB license banner to all pages
    - Create CCBBanner component showing contractor license information
    - Display OR CCB# 259926 and WA LIC# SFWCOC*745BH
    - Add SFW Construction link to sfwconstruction.com
    - Position banner above footer on all pages
    - Dynamically displays DBA name from config

--- packages/ui/src/components/layout/index.ts
+++ packages/ui/src/components/layout/index.ts
@@ -5,7 +5,9 @@
 export { default as Header } from './Header.astro';
 export { default as Footer } from './Footer.astro';
 export { default as MobileNav } from './MobileNav.astro';
+export { default as CCBBanner } from './CCBBanner.astro';
 
 export type { Props as HeaderProps } from './Header.astro';
 export type { Props as FooterProps } from './Footer.astro';
 export type { Props as MobileNavProps } from './MobileNav.astro';
+export type { Props as CCBBannerProps } from './CCBBanner.astro';
