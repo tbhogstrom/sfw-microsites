c98981e2f46edc5c65339ea9a4aa92ce9815708a
Tyler Falcon <tfalcon@sfwconstruction.com>
2026-03-27 09:05:23 -0700

    refactor(ui): split HeroSection into composable sub-components

--- packages/ui/src/components/hero/index.ts
+++ packages/ui/src/components/hero/index.ts
@@ -4,6 +4,12 @@
 
 export { default as HeroSection } from './HeroSection.astro';
 export { default as HeroWithForm } from './HeroWithForm.astro';
+export { default as HeroLayout } from './HeroLayout.astro';
+export { default as HeroContent } from './HeroContent.astro';
+export { default as HeroForm } from './HeroForm.astro';
 
 export type { Props as HeroSectionProps } from './HeroSection.astro';
 export type { Props as HeroWithFormProps } from './HeroWithForm.astro';
+export type { Props as HeroLayoutProps } from './HeroLayout.astro';
+export type { Props as HeroContentProps } from './HeroContent.astro';
+export type { Props as HeroFormProps } from './HeroForm.astro';
